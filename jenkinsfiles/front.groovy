24a97d8ce9321c999bc32863cc144689f7626ca7
So Jeong Park <psj6414@naver.com>
2021-03-13 10:45:20 +0900

    modify : pipeline change

--- jenkinsfiles/front.groovy
+++ jenkinsfiles/front.groovy
@@ -1,28 +1,54 @@
-pipeline {
-    agent any
+import org.jenkinsci.plugins.workflow.libs.Library
 
-    tools {nodejs "node"}
+import static java.util.concurrent.TimeUnit.SECONDS
 
-    stages {
+@Library('shared') _
+/**
+ * @author Park So Jeong
+ */
 
-        stage('Git') {
-            steps {
-                git 'https://github.com/LeeJaeDoo/sp-fe.git'
-            }
-        }
-
-        stage('Build') {
-            steps {
-                sh 'npm install'
-                sh '<<Build Command>>'
-            }
-        }
-
-
-        stage('Test') {
-            steps {
-                sh 'node test'
-            }
-        }
+node {
+    stage('CHECKOUT') {
+        gitCheckout()
     }
+//    stage ('CLEAN') {
+//        clean()
+//    }
+    stage('BUILD') {
+        build()
+    }
+//    stage ('TEST') {
+//        test()
+//    }
+    stage('DEPLOY') {
+        deploy()
+    }
+}
+def gitCheckout() {
+    checkout([$class                           : 'GitSCM',
+              branches                         : [[name: "*/master"]],
+              doGenerateSubmoduleConfigurations: false,
+              extensions                       : [[$class: 'WipeWorkspace'], [$class: 'LocalBranch', localBranch: '**']],
+              userRemoteConfigs                : [[credentialsId: 'LeeJaeDoo', url: 'https://LeeJaeDoo:262283b0c7139420fc2eb4f2d2a6b0e8d6fc19d4@github.com/LeeJaeDoo/sp-fe']]])
+
+    log.withStepNoEnd(stageName: "최근 commit log", {
+        sh script: "git log --since=" + lastBuild.diffTime(SECONDS) + ".seconds --pretty=format:'%h %<(10,trunc)%an %ad   %s' --date=format:'%Y-%m-%d %H:%M'"
+    })
+
+    log.withStepNoEnd(stageName: "최근 changed log", {
+        sh script: "git diff \$(git log -1 --before=@{" + lastBuild.diffTime(SECONDS) + ".seconds.ago} --format=%H) --stat"
+    })
+}
+
+def build(String subProjectName = "", String additionalOption = "") {
+    sh script: "sudo npm install"
+    sh script: "sudo npm run build"
+}
+
+def deploy(String playbookName = "", String  extras = "") {
+    ansiblePlaybook(
+            inventory: "/home/ubuntu/ansibles/inventories/hosts/inventory",
+            playbook: "/home/ubuntu/ansibles/sp-fe.yml",
+            extras: "--ssh-common-args='-o StrictHostKeyChecking=no' "
+    )
 }
