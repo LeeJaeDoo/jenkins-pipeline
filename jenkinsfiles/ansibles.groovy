1d41bb5d77f0fd6cdf66e8554e712e22735c3c92
Jaedoo.Lee <dlwoen9@godo.co.kr>
2021-05-31 23:48:05 +0900

    ansible git path 수정

--- jenkinsfiles/ansibles.groovy
+++ jenkinsfiles/ansibles.groovy
@@ -13,5 +13,5 @@ node {
 
 def gitPull() {
   sh label: '', script: '''cd /home/ubuntu/ansibles
-  git pull https://LeeJaeDoo:262283b0c7139420fc2eb4f2d2a6b0e8d6fc19d4@github.com/LeeJaeDoo/ansibles.git master'''
+  git pull https://LeeJaeDoo:262283b0c7139420fc2eb4f2d2a6b0e8d6fc19d4@github.com/it-chip/ansibles.git master'''
 }
